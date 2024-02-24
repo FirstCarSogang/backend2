6da5be8c3d0fa1d0ea823a0d12bca9c177951d51
acupoframen <choe.hwan@gmail.com>
2024-02-24 11:33:09 +0900

    updated urls.py

--- backend/firstCarSogang_backend/urls.py
+++ backend/firstCarSogang_backend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firstCarSogang_signuplogin.urls')),
+    path('', include('firstCarSogang_tickets.urls'))
 ]
